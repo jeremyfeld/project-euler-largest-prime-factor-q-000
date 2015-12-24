688d6c0d1a244ab4fc534a95b028fadcebbca913
jeremyfeld <jeremyfeld@gmail.com>
2015-12-24 16:14:03 +0000

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,9 @@
 # Enter your procedural solution here!
+def prime_factors(input)
+  require 'prime'
+  Prime.prime_division(input)
+end
+
+def largest_prime_factor(input)
+  prime_factors(input).last.at(0)
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,15 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  def initialize(input)
+    @input = input
+    require 'prime'
+  end
+
+  def prime_factors
+    Prime.prime_division(@input)
+  end
+
+  def number
+    prime_factors.last.at(0)
+  end
+end
